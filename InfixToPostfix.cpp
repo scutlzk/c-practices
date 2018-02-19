28294688cf71b5e769094d6ff9cb765d75075052
scuter_lzk <l1005152@gmail.com>
2018-02-19 18:07:02 +0800

    算术表达式中缀转后缀

---
+++ InfixToPostfix.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+#include<stack>
+#include<sstream>
+using namespace std;
+string InfixToPostfix(string infix) 
+{	
+	stringstream a(infix);
+	stack<char> op;
+	string postfix;
+	int n;
+	char t;
+	a >> n;		//式子第一个肯定是数字
+
+	postfix += to_string(n);
+	while (a >> t)
+	{
+		if (op.size() && (t == '+' || t == '-'))
+		{
+			postfix += string(" ") + op.top();
+			op.pop();
+		}
+		op.push(t);
+
+		if (a >> t&&t == '(')
+		{
+			string x;
+			a >> t;
+			while (t != ')')
+			{
+				x += t;
+				a >> t;
+			}
+			postfix += string(" ") + InfixToPostfix(x);
+		}
+		else
+		{
+			a.putback(t);
+			a >> n;
+			postfix += string(" ") + to_string(n);
+		}
+
+		if (op.size() && (op.top() == '*' || op.top() == '/'))
+		{
+			postfix += string(" ") + op.top();
+			op.pop();
+
+		}
+	}
+	while (op.size()) { postfix += string(" ") + op.top(); op.pop(); }
+	return postfix;
+}
+
+int main()
+{	string infix;
+	while (1)
+	{
+		getline(cin,infix);
+		cout<<InfixToPostfix(infix)<<endl;
+	}
+}
